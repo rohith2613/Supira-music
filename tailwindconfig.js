686f93fc63aa4294461e713f4263863ac72577e0
Rohith <rohithsriramula2613@gmail.com>
2023-12-26 14:53:14 +0530

    first commit

---
+++ tailwindconfig.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme:{
+    extend: {
+      colors :{
+        'logo' : {
+          
+        700: '#0e7490',
+        800: '#155e75',
+        
+        }
+      }
+    }
+  }
+
+}
